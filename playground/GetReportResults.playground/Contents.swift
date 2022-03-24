c4c68cdef32b68de02fc92afd5bc1de5249e09da
EJinK0 <49181825+EJinK0@users.noreply.github.com>
2022-03-24 21:19:45 +0900

    신고결과받기3

--- playground/GetReportResults.playground/Contents.swift
+++ playground/GetReportResults.playground/Contents.swift
@@ -13,25 +13,20 @@ func solution(_ id_list: [String], _ report:[String], _ k:Int) -> [Int] {
     var reportResult_name: [String: [String]] = [String: [String]]()
     var reportResult_count: [String: Int] = [String: Int]()
     var beStopped: [String] = [String]()
-    var i: Int = 0
-    
-    for _ in id_list {
-        reportResult_name[id_list[i]] = []
-        reportResult_count[id_list[i]] = 0
-        i += 1
-    }
+    var notiCounts : [String: Int] = [String: Int]()
+    var j: Int = 0
 
     for data in report {
         let names = data.split(separator: " ").map { String($0) }
         let whoReport: String = names[0]
         let personReported: String = names[1]
         
-        var reportedPerson: [String] = reportResult_name[whoReport]!
+        var reportedPerson: [String] = reportResult_name[whoReport] ?? []
         if(!reportedPerson.contains(personReported)) {
             reportedPerson.append(personReported)
             reportResult_name[whoReport] = reportedPerson
             
-            var reportedCount: Int = reportResult_count[personReported]!
+            var reportedCount: Int = reportResult_count[personReported] ?? 0
             reportedCount += 1
             reportResult_count[personReported] = reportedCount
             
@@ -41,26 +36,21 @@ func solution(_ id_list: [String], _ report:[String], _ k:Int) -> [Int] {
         }
     }
     
-    var notiCounts : [String: Int] = [String: Int]()
-    var k: Int = 0
-    
     for (key, value) in reportResult_name {
         let ids: [String] = value
         for name in ids {
             if beStopped.contains(name) {
-                k += 1
+                j += 1
             }
         }
-        notiCounts[key] = k
-        k = 0
+        notiCounts[key] = j
+        j = 0
     }
     
     var result: [Int] = [Int]()
     for id in id_list {
-        result.append(notiCounts[id]!)
+        result.append(notiCounts[id] ?? 0)
     }
-    
-    print(result)
 
     return result
 }
