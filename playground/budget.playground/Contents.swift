a49089ee08094dc34deff19238086f4777e41e54
EJinK0 <49181825+EJinK0@users.noreply.github.com>
2022-04-11 23:56:14 +0900

    예산

--- playground/budget.playground/Contents.swift
+++ playground/budget.playground/Contents.swift
@@ -10,47 +10,20 @@ var d3: [Int] = [5,5]
 var budget3: Int = 9
 
 
-solution(d3, budget3)
+solution(d, budget)
 
 func solution(_ d:[Int], _ budget:Int) -> Int {
-    var result: [[Int]] = [[Int]]()
-    var max: Int = 0
-    var isMax: Bool = false
+    var money = budget
     
-    for i in stride(from:d.count, to:0, by: -1) {
-        getCombination(d, i, &result)
-        print("\(i) \(result)")
-        for j in 0..<result.count {
-            let sum: Int = result[j].reduce(0, {(result: Int, next: Int) -> Int in
-                return result + next
-            })
-            if sum <= budget {
-                max = i
-                isMax = true
-            }
-            
+    let result = d.sorted().filter {
+        if (money - $0) >= 0 {
+            money = money - $0
+            return true
+        } else {
+            return false
         }
-        result.removeAll()
-        
-        if isMax == true {
-            break
-        }
-    }
+    }.count
     
-    print(max)
-    return max
-}
-
-func getCombination<T>(_ arr: [T], _ combiNum: Int, _ result: inout [[T]], _ now: [T] = [T]()) {
-    let arrayCount = arr.count
-    guard arrayCount > 0 else { return }
-    
-    if combiNum == 0 {
-        result.append(now)
-    } else if arrayCount == combiNum {
-        result.append(now + arr)
-    } else {
-        getCombination(Array(arr[1...]), combiNum - 1, &result, now + [arr.first!])
-        getCombination(Array(arr[1...]), combiNum, &result, now)
-    }
+    print(result)
+    return result
 }
