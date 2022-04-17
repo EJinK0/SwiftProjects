39bc4a86825404017c41da37de7ac280a60fa9dd
EJinK0 <49181825+EJinK0@users.noreply.github.com>
2022-04-17 18:25:32 +0900

    거리두기
    1. 맨해튼 거리 2 이하인 배열에서 2개씩 검사
    2. 각각의 point가 같은 row에 위치할때, 같은 column 일때, 대각선에 위치할때 거리두기를 유지했는지 검사
    3. 맨해튼 거리가 1이거나 유지를 하지못했을때는 해당 room에 result 값을 0으로 넣기

--- CheckTheDistance.playground/Contents.swift
+++ CheckTheDistance.playground/Contents.swift
@@ -12,8 +12,9 @@ solution(places)
 func solution(_ places:[[String]]) -> [Int] {
     var person: [(Int, Int)] = [(Int, Int)]()
     var manhattanTwoUnder: [(Int, Int)] = [(Int, Int)]()
-    var result: [Int] = [Int]()
+    var result: [Int] = [Int](repeatElement(0, count: 5))
     var isKeepDistance: Bool = true
+    var isManhattanDistanceOne = false
     
     for (i,room) in places.enumerated() {
         //print("\(i) \(room)")
@@ -27,10 +28,10 @@ func solution(_ places:[[String]]) -> [Int] {
             }
         }
         
-        print("person: \(person)")
-        
         if person.count == 0 {
-            result.append(1)
+            print("this room is no person")
+            print("-----------------------")
+            result[i] = 1
             continue
         }
         
@@ -43,33 +44,93 @@ func solution(_ places:[[String]]) -> [Int] {
                 }
             }
         }
-
-        print("manhattanTwoUnder \(manhattanTwoUnder))")
         
+        print("manhattanTwoUnder roomnum is \(manhattanTwoUnder)")
+        
+        var tempManhattanTwoUnder: [(Int, Int)] = [(Int, Int)]()
         
         var count = 0;
-        for (i,(x,y)) in manhattanTwoUnder.enumerated() {
-            if count == 1 {
-                count = 0
+        for (j,(x,y)) in manhattanTwoUnder.enumerated() {
+            if j+1 == manhattanTwoUnder.count {
                 continue
             }
-            var next: (Int,Int) = manhattanTwoUnder[i+1]
-            if (places[x][next.1] != "X" && places[x][next.1] != "P")
-                || (places[next.0][y] != "X" && places[next.0][y] != "P") {
-                result.append(0)
-                isKeepDistance = false
+            
+            if j % 2 != 0 {
+                continue
             }
-
-            if !isKeepDistance {
+            
+            let next: (Int,Int) = manhattanTwoUnder[j+1]
+            
+            if abs(x - next.0) + abs(y - next.1) == 1 {
+                isManhattanDistanceOne = true
                 break
             }
+            
+            if x == next.0 {
+                var temp: String = room[x]
+                var tempItem: String = ""
 
-            count += 1
+                for (i,char) in temp.enumerated() {
+                    if next.1 - 1 == i {
+                        tempItem = String(char)
+                    }
+                }
+                
+                if tempItem != "X" {
+                    isKeepDistance = false
+                    break
+                }
+            } else if(y == next.1) {
+                var temp2: String = room[next.0-1]
+                var tempItem2: String = ""
+                for (j,char) in temp2.enumerated() {
+                    if j == y {
+                        tempItem2 = String(char)
+                    }
+                }
+                if tempItem2 != "X" {
+                    isKeepDistance = false
+                    break
+                }
+            } else {
+                var temp3: String = room[x]
+                var temp4: String = room[next.0]
+                var tempItem3: String = ""
+                var tempItem4: String = ""
+                
+                for (i,char) in temp3.enumerated() {
+                    if i == next.1 {
+                        tempItem3 = String(char)
+                    }
+                }
+                
+                for(j,char) in temp4.enumerated() {
+                    if j == y {
+                        tempItem4 = String(char)
+                    }
+                }
+                
+                if tempItem3 != "X" || tempItem4 != "X" {
+                    isKeepDistance = false
+                    break
+                }
+            }
         }
 
-        if isKeepDistance{
-            result.append(1)
+        print("isManhattanDistanceOne \(isManhattanDistanceOne), isKeepDistance \(isKeepDistance)")
+        if isManhattanDistanceOne || !isKeepDistance {
+            result[i] = 0
+            isManhattanDistanceOne = false
+            isKeepDistance = true
+            print("this room is manhattan distance one of false")
+            print("---------------")
+            person.removeAll()
+            manhattanTwoUnder.removeAll()
+            continue
         }
+    
+        result[i] = 1
+        print("this room is keep distance true!!")
         
         person.removeAll()
         manhattanTwoUnder.removeAll()
@@ -78,5 +139,5 @@ func solution(_ places:[[String]]) -> [Int] {
     }
     
     print(result)
-    return []
+    return result
 }
