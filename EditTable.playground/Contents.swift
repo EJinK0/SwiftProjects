bfb6cf369cfe38cd44bf66a6e20867a73d14f510
EJinK0 <49181825+EJinK0@users.noreply.github.com>
2022-04-19 01:32:17 +0900

    표 편집
    1. c 했을때 indexing의 selectedidx에다가 -1 값을 넣는다
    2. u 나 d을 할때 indexing값이 -1값이면 continue
    3. z를 하면 indexing에 해당 값을 다시 집어넣는다

--- EditTable.playground/Contents.swift
+++ EditTable.playground/Contents.swift
@@ -2,9 +2,12 @@ import UIKit
 import Foundation
 
 var n: Int = 8
+var n2: Int = 5
 var k: Int = 2
+var k2: Int = 1
 var cmd: [String] = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]
 var cmd2: [String] = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]
+var cmd3: [String] = ["C", "C", "C"]
 
 solution(n, k, cmd2)
 
@@ -19,41 +22,92 @@ func solution(_ n:Int, _ k:Int, _ cmd:[String]) -> String {
         origin.append("O")
     }
     
-    print(selectedIdx)
+    print(indexing)
     
     for command in cmd {
+        print(command)
         var split = command.split(separator: " ")
-        var tempX: Int = 0
         if split[0] == "U" {
-            tempX = Int(split[1]) ?? 0
-            selectedIdx -= tempX
-            print("moved idx is \(selectedIdx)")
-        } else if split[0] == "D" {
-            tempX = Int(split[1]) ?? 0
-            selectedIdx += tempX
-            print("moved idx is \(selectedIdx)")
-        } else if split[0] == "C" {
-            indexing.remove(at: selectedIdx)
-            deletedIdx.append(selectedIdx)
-            print("deleted idx is \(selectedIdx)")
-            if selectedIdx == indexing.count {
+            let num = Int(split[1])
+            var count = 1
+            var isNegative: Bool = false
+            while !isNegative {
                 selectedIdx -= 1
-                print("idx is \(selectedIdx)")
+                if num == 1 {
+                    if indexing[selectedIdx] == -1 {
+                        continue
+                    } else {
+                        isNegative = true
+                        continue
+                    }
+                } else {
+                    if count == num {
+                        isNegative = true
+                        continue
+                    }
+                    
+                    if indexing[selectedIdx] == -1 {
+                        continue
+                    } else {
+                        count += 1
+                        continue
+                    }
+                }
             }
+        } else if split[0] == "D" {
+            let num = Int(split[1])
+            var count = 1
+            var isNegative: Bool = false
+            while !isNegative {
+                selectedIdx += 1
+                if num == 1 {
+                    if indexing[selectedIdx] == -1 {
+                        continue
+                    } else {
+                        isNegative = true
+                        continue
+                    }
+                } else {
+                    if count == num {
+                        isNegative = true
+                        continue
+                    }
+                    
+                    if indexing[selectedIdx] == -1 {
+                        continue
+                    } else {
+                        count += 1
+                        continue
+                    }
+                }
+            }
+        } else if split[0] == "C" {
+            indexing[selectedIdx] = -1
+            deletedIdx.append(selectedIdx)
+            if selectedIdx == indexing.count - 1 {
+                selectedIdx -= 1
+                print(selectedIdx)
+                continue
+            }
+            selectedIdx += 1
         } else if split[0] == "Z" {
-            print("deleted list \(deletedIdx)")
-            var deletedListLastItem = deletedIdx.removeLast()
-            indexing.insert(deletedListLastItem, at: deletedListLastItem)
-            print("recover item index is \(deletedListLastItem)")
+            var lastItem = deletedIdx.removeLast()
+            indexing[lastItem] = lastItem
+            //print(selectedIdx)
         }
         
-        print("cmd \(command)/ selected idx:\(selectedIdx)/ indexlist\(indexing)")
-        print("--------")
+        print(selectedIdx)
     }
     
-    for item in deletedIdx {
-        origin[item] = "X"
+    var result: String = ""
+    for item in indexing {
+        if item == -1 {
+            result += "X"
+        } else {
+            result += "O"
+        }
     }
-    print(origin)
-    return ""
+    print(indexing)
+    print(result)
+    return result
 }
