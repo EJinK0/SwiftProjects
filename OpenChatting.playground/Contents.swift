d0a21c59ce29ad017e68b58b36ec8745692e4105
EJinK0 <49181825+EJinK0@users.noreply.github.com>
2022-04-29 00:05:24 +0900

    오픈채팅방
    for문을 두번 돌려서 첫번째 for문은 새로운 닉네임 업데이트
    두번째 for문은 해당 닉네임으로 enter, leave 알림메시지 출력

--- OpenChatting.playground/Contents.swift
+++ OpenChatting.playground/Contents.swift
@@ -9,41 +9,27 @@ var record: [String] = ["Enter uid1234 Muzi",
 solution(record)
 
 func solution(_ record:[String]) -> [String] {
-    //var result: [String] = []
-    var notification: [(String, String)] = [(String, String)]()
-    var nowNickName: [String: String] = [String: String]()
+    var notification: [String] = []
+    var nowNickName: [String: String] = [:]
     
     for noti in record {
         let command: [String] = noti.components(separatedBy: " ")
         
-        if command[0] == "Enter" {
-            notification.append((command[1], "\(command[2])님이 들어왔습니다."))
-            
-            if nowNickName[command[1]] == nil {
-                nowNickName[command[1]] = nowNickName[command[2]]
-            } else {
-                if nowNickName[command[1]] != command[2] {
-                    for (i,noti) in notification.enumerated() {
-                        if noti.0 == command[1] {
-                            var notiSplit = noti.1.components(separatedBy: "님")
-                            notification[i].1 = "\(command[2])님\(notiSplit[1])"
-                        }
-                    }
-                }
-            }
-        } else if command[0] == "Leave" {
-            notification.append((command[1], "\(nowNickName[command[1]])님이 나갔습니다."))
-            
-        } else if command[0] == "Change" {
-            for (i,noti) in notification.enumerated() {
-                if noti.0 == command[1] {
-                    var notiSplit = noti.1.components(separatedBy: "님")
-                    notification[i].1 = "\(command[2])님\(notiSplit[1])"
-                }
-            }
+        if command.count > 2 {
+            nowNickName[command[1]] = command[2]
         }
     }
     
-    print(notification)
-    return []
+    for noti in record {
+        let command: [String] = noti.components(separatedBy: " ")
+        let nickName = nowNickName[command[1]]!
+        
+        if command[0] == "Enter" {
+            notification.append("\(nickName)님이 들어왔습니다.")
+        } else if command[0] == "Leave" {
+            notification.append("\(nickName)님이 나갔습니다.")
+        }
+    }
+
+    return notification
 }
